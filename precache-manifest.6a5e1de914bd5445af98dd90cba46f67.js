839429279f27d3d4f5ee46bee1d71f6fd04265be
Timothy Merritt <timmybytes@gmail.com>
2020-09-13 12:25:34 -0700

    Updates

---
+++ precache-manifest.6a5e1de914bd5445af98dd90cba46f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61311a9dfab806538356e5366843062",
+    "url": "/color-chooser/index.html"
+  },
+  {
+    "revision": "0c83600ac6cbedccb1c9",
+    "url": "/color-chooser/static/css/main.00726855.chunk.css"
+  },
+  {
+    "revision": "5ba1d7988c44b5db0765",
+    "url": "/color-chooser/static/js/2.22c0c28a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-chooser/static/js/2.22c0c28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c83600ac6cbedccb1c9",
+    "url": "/color-chooser/static/js/main.199443bf.chunk.js"
+  },
+  {
+    "revision": "1c466d00e77dfae7ddf6",
+    "url": "/color-chooser/static/js/runtime-main.3518d3dc.js"
+  }
+]);
